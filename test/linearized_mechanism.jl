2f40d1bf8fd26fb5e30345913f837487e1364c8e
Robin Deits <robin.deits@gmail.com>
2017-08-24 18:07:40 -0400

    more tests for linearization

--- test/linearized_mechanism.jl
+++ test/linearized_mechanism.jl
@@ -8,15 +8,50 @@ srand(1)
 @testset "linearized mechanism" begin
     @testset "truly linear mechanism" begin
         mechanism = rand_chain_mechanism(Float64, [Prismatic{Float64} for i = 1:5]...)
+        x0 = MechanismState{Float64}(mechanism)
+        x = MechanismState{Float64}(mechanism)
         for i in 1:10
-            x0 = MechanismState(mechanism, randn(num_positions(mechanism)), randn(num_velocities(mechanism)))
-            x = MechanismState(mechanism, randn(num_positions(mechanism)), randn(num_velocities(mechanism)))
+            set_configuration!(x0, randn(num_positions(x0)))
+            set_velocity!(x0, randn(num_velocities(x0)))
+            set_configuration!(x, randn(num_positions(x0)))
+            set_velocity!(x, randn(num_velocities(x0)))
             x_linear = MechanismComplementarity.Linear.LinearizedState(x0, state_vector(x))
             @test center_of_mass(x) ≈ lineval(center_of_mass, x_linear)
+            @test !(center_of_mass(x) ≈ center_of_mass(x0))
 
             for body in bodies(mechanism)
                 frame = default_frame(body)
                 @test transform_to_root(x, frame) ≈ lineval(x -> transform_to_root(x, frame), x_linear)
+
+                p = Point3D(frame, randn(3))
+                @test transform_to_root(x, frame) * p ≈ lineval(x -> transform_to_root(x, frame) * p, x_linear)
+            end
+        end
+    end
+
+    @testset "nonlinear mechanism" begin
+        mechanism = rand_chain_mechanism(Float64, [QuaternionFloating{Float64}; [Revolute{Float64} for i = 1:5]]...)
+        x0 = MechanismState{Float64}(mechanism)
+        x = MechanismState{Float64}(mechanism)
+        for i in 1:10
+            set_configuration!(x0, randn(num_positions(x0)))
+            set_velocity!(x0, randn(num_velocities(x0)))
+
+            set_configuration!(x, configuration(x0) + 1e-5 * (rand(num_positions(x0)) .- 0.5))
+            set_velocity!(x, velocity(x0) + 1e-5 * (rand(num_velocities(x0)) .- 0.5))
+
+            x_linear =  MechanismComplementarity.Linear.LinearizedState(x0, state_vector(x))
+
+            @test center_of_mass(x) ≈ lineval(center_of_mass, x_linear)
+            @test !(center_of_mass(x0) ≈ center_of_mass(x))  # make sure we've perturbed enough for linearization to help
+
+            for (i, body) in enumerate(bodies(mechanism))
+                frame = default_frame(body)
+                @test transform_to_root(x, frame) ≈ lineval(x -> transform_to_root(x, frame), x_linear)
+
+                if i > 1
+                    @test !(transform_to_root(x0, frame) ≈ transform_to_root(x, frame))
+                end
 
                 p = Point3D(frame, randn(3))
                 @test transform_to_root(x, frame) * p ≈ lineval(x -> transform_to_root(x, frame) * p, x_linear)
