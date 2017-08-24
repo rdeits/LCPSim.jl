5ab600f6ba73729af731b71755fa255313fc2172
Robin Deits <robin.deits@gmail.com>
2017-08-24 18:08:47 -0400

    cleanup

--- test/linearized_mechanism.jl
+++ test/linearized_mechanism.jl
@@ -59,19 +59,3 @@ srand(1)
         end
     end
 end
-
-
-    # world = RigidBody{Float64}("world")
-    # brick = Mechanism(world; gravity=SVector(0, 0, -9.81))
-
-    # frame = CartesianFrame3D("dummy")
-    # inertia = SpatialInertia(frame, 0 * eye(3), zeros(3), 0.0)
-    # dummy = RigidBody(inertia)
-    # base_x = Joint("base_x", Prismatic([1., 0, 0]))
-    # attach!(brick, world, base_x, eye(Transform3D, frame_before(base_x), default_frame(world)), dummy)
-
-    # frame = CartesianFrame3D("core")
-    # inertia = SpatialInertia(frame, 0.1 * eye(3), zeros(3), 1.0)
-    # core = RigidBody(inertia)
-    # base_z = Joint("base_z", Prismatic([0., 0, 1]))
-    # attach!(brick, dummy, base_z, eye(Transform3D, frame_before(base_z), default_frame(dummy)), core)
