22e22af19c5c50e17e9a7341d87821a0282da084
Robin Deits <robin.deits@gmail.com>
2017-09-14 15:25:22 -0400

    fix setvalue for LCPUpdate

--- src/variable_containers.jl
+++ src/variable_containers.jl
@@ -1,3 +1,4 @@
+using JuMP: AbstractJuMPScalar
 
 struct ContactResult{T, M}
     β::Vector{T}
@@ -23,13 +24,13 @@ LCPUpdate(state::StateRecord{T}, input::I, contacts::Associative{<:RigidBody{M}}
     LCPUpdate{T, M, I}(state, input, contacts, joint_contacts)
 
 _getvalue(x::AbstractVector{<:Number}) = x
-_getvalue(x::AbstractVector{<:JuMP.AbstractJuMPScalar}) = getvalue(x)
+_getvalue(x::AbstractVector{<:AbstractJuMPScalar}) = getvalue(x)
 
 
 JuMP.getvalue(r::JointLimitResult) = JointLimitResult(getvalue(r.λ), r.direction)
-# JuMP.getvalue(x::MechanismState{<:JuMP.AbstractJuMPScalar}) = 
+# JuMP.getvalue(x::MechanismState{<:AbstractJuMPScalar}) = 
 #     MechanismState(x.mechanism, getvalue(configuration(x)), getvalue(velocity(x)), getvalue(additional_state(x)))
-JuMP.getvalue(r::StateRecord{<:JuMP.AbstractJuMPScalar}) = StateRecord(r.mechanism, getvalue.(r.state))
+JuMP.getvalue(r::StateRecord{<:AbstractJuMPScalar}) = StateRecord(r.mechanism, getvalue.(r.state))
 # JuMP.getvalue(p::Pair{<:RigidBody, <:AbstractVector{<:ContactResult}}) = p.first => getvalue.(p.second)
 # JuMP.getvalue(p::Pair{<:Joint, <:AbstractVector{<:JointLimitResult}}) = p.first => getvalue.(p.second)
 JuMP.getvalue(up::LCPUpdate) =
@@ -41,24 +42,33 @@ JuMP.getvalue(up::LCPUpdate) =
 JuMP.getvalue(c::ContactResult) = ContactResult(getvalue.((c.β, c.λ, c.c_n))..., c.point, c.obs)
 JuMP.getvalue(f::FreeVector3D) = FreeVector3D(f.frame, getvalue(f.v))
 
-function JuMP.setvalue(r::JointLimitResult{<:JuMP.AbstractJuMPScalar}, seed::JointLimitResult{<:Number})
+function JuMP.setvalue(r::JointLimitResult{<:AbstractJuMPScalar}, seed::JointLimitResult{<:Number})
     setvalue(r.λ, seed.λ)
-    @assert getvalue(r.generalized_force) ≈ seed.generalized_force
 end
 
 function JuMP.setvalue(d::Dict{<:Joint, <:Vector{<:JointLimitResult}}, seed::Dict)
     [setvalue.(v1, v2) for (v1, v2) in zip(values(d), values(seed))]
 end
 
-function JuMP.setvalue(up::LCPUpdate{<:JuMP.AbstractJuMPScalar}, seed::LCPUpdate{<:Number})
+function JuMP.setvalue(up::LCPUpdate{<:AbstractJuMPScalar}, seed::LCPUpdate{<:Number})
     setvalue(up.state, seed.state)
     setvalue.(up.contacts, seed.contacts)
     setvalue(up.joint_contacts, seed.joint_contacts)
 end
 
-function JuMP.setvalue(contact::ContactResult{<:JuMP.AbstractJuMPScalar}, seed::ContactResult{<:Number})
+function JuMP.setvalue(contact::ContactResult{<:AbstractJuMPScalar}, seed::ContactResult{<:Number})
     @assert contact.obs == seed.obs
     setvalue(contact.β, seed.β)
     setvalue(contact.λ, seed.λ)
     setvalue(contact.c_n, seed.c_n)
+end
+
+function JuMP.setvalue(contacts::Dict{<:RigidBody, <:Vector{<:ContactResult}}, seeds::Dict)
+    for body in keys(contacts)
+        setvalue.(contacts[body], seeds[body])
+    end
+end
+
+function JuMP.setvalue(state::StateRecord{<:AbstractJuMPScalar}, seed::StateRecord{<:Number})
+    setvalue.(state.state, seed.state)
 end
