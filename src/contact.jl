8564958080723af48a34fc1ce7af10b020296227
Robin Deits <robin.deits@gmail.com>
2017-12-01 18:35:08 -0500

    random attempts at velocity formulation of lcp

--- src/contact.jl
+++ src/contact.jl
@@ -1,20 +1,20 @@
 
 function contact_basis(obs::Obstacle{T}) where T
     a::SVector{3,T} = normalize(SVector{3, T}(obs.contact_face.a))
-    # D = SVector{2}([
-    #     FreeVector3D(obs.frame, Rotations.RotY(π/2) * a),
-    #     FreeVector3D(obs.frame, Rotations.RotY(-π/2) * a)
-    #     ])
-    R = Rotations.rotation_between(SVector{3, T}(0, 0, 1), a)
-    D = SVector{4}([
-        FreeVector3D(obs.frame, R * RotZ(θ) * SVector(1, 0, 0))
-        for θ in 0:π/2:3π/2])
-    for i in 1:length(D)
-        @assert isapprox(dot(D[i].v, obs.contact_face.a), 0, atol=1e-15)
-        if i > 1
-            @assert isapprox(dot(D[i].v, D[i-1].v), 0, atol=1e-15)
-        end
-    end
+    D = SVector{2}([
+        FreeVector3D(obs.frame, Rotations.RotY(π/2) * a),
+        FreeVector3D(obs.frame, Rotations.RotY(-π/2) * a)
+        ])
+    # R = Rotations.rotation_between(SVector{3, T}(0, 0, 1), a)
+    # D = SVector{4}([
+    #     FreeVector3D(obs.frame, R * RotZ(θ) * SVector(1, 0, 0))
+    #     for θ in 0:π/2:3π/2])
+    # for i in 1:length(D)
+    #     @assert isapprox(dot(D[i].v, obs.contact_face.a), 0, atol=1e-15)
+    #     if i > 1
+    #         @assert isapprox(dot(D[i].v, D[i-1].v), 0, atol=1e-15)
+    #     end
+    # end
     D
 end
 
@@ -62,7 +62,7 @@ function append(v::AbstractVector{T}, x::T) where T
     result
 end
 
-function add_contact_constraints(model::Model, point, obstacle, β, λ, c_n, D_transpose_times_v, separation_from_obstacle, Δr, Δr_true)
+function add_contact_constraints(model::Model, point, obstacle, β, λ, c_n, D_transpose_times_v, separation_from_obstacle, normal_velocity, Δr, Δr_true)
 
     @constraints model begin
         separation_from_obstacle >= 0 # (7)
@@ -73,10 +73,17 @@ function add_contact_constraints(model::Model, point, obstacle, β, λ, c_n, D_t
         @constraint model λ + d >= 0 # (8)
     end
 
-    @disjunction(model, 
-        ((separation_from_obstacle <= 1e-3) & (Δr.v .== Δr_true.v)),
-        ((c_n == 0) & (Δr.v .== 0))
-    ) # (10)
+    @disjunction(model,
+                 (separation_from_obstacle <= 0) & (Δr.v .== Δr_true.v),
+                 (c_n <= 0) & (Δr.v .== 0))
+
+
+    # @disjunction(model,
+    #              ((c_n == 0) & (normal_velocity >= 0)),
+    #              ((normal_velocity == 0) & (separation_from_obstacle <= 0)),
+    #              ((c_n == 0) & (separation_from_obstacle >= 0))
+    #              )
+
     for j in 1:length(D_transpose_times_v)
         d = D_transpose_times_v[j]
         λ_plus_d = AffExpr(append(d.vars, λ), append(d.coeffs, 1.0), d.constant)
@@ -104,43 +111,52 @@ function resolve_contact(xnext::LinearizedState, body::RigidBody, point::Point3D
     end
 
     # contact_velocity = linearized(x -> point_velocity(twist_wrt_world(x, body), transform_to_root(linearization_state(xnext), point.frame) * point), xnext)
+    normal_velocity = linearized(xnext) do x
+        dot(contact_normal(obstacle), point_velocity(twist_wrt_world(x, body), transform_to_root(linearization_state(xnext), point.frame) * point))
+    end
+
     # D_transpose_times_v = map(D) do d
     #     dot(d, contact_velocity)
     # end
 
-    D_transpose_times_v = [
-        linearized(x -> dot(d, point_velocity(twist_wrt_world(x, body), transform_to_root(linearization_state(xnext), point.frame) * point)), xnext) for d in D
-        ]
-
-    add_contact_constraints(model, point, obstacle, β, λ, c_n, D_transpose_times_v, separation_from_obstacle, Δr, Δr_true)
-end
-
-function add_contact_constraints_nonsliding(model::Model, point, obstacle, β, λ, c_n, D, separation_from_obstacle, contact_velocity)
-    D_transpose_times_v = [dot(d, contact_velocity) for d in D]
-
-    @constraints model begin
-        separation_from_obstacle >= 0 # (7)
-        # λ .+ D_transpose_times_v .>= 0 # (8)
-        obstacle.μ * c_n .- sum(β) >= 0 # (9)
+    D_transpose_times_v = map(D) do d
+        linearized(xnext) do x
+            dot(d, point_velocity(twist_wrt_world(x, body), transform_to_root(linearization_state(xnext), point.frame) * point))
+        end
     end
+    # D_transpose_times_v = [
+    #     linearized(x -> dot(d, point_velocity(twist_wrt_world(x, body), transform_to_root(linearization_state(xnext), point.frame) * point)), xnext) for d in D
+    #     ]
 
-    @disjunction(model, 
-                 (&)(separation_from_obstacle == 0, 
-                     [@?(contact_velocity.v[i] == 0) for i in 1:3]...),
-                 c_n == 0)
-    ContactResult(β, λ, c_n, point, obstacle)
+    add_contact_constraints(model, point, obstacle, β, λ, c_n, D_transpose_times_v, separation_from_obstacle, normal_velocity, Δr, Δr_true)
 end
 
-function add_contact_constraints_sticking(model::Model, point, obstacle, β, λ, c_n, D, separation_from_obstacle, contact_velocity)
+# function add_contact_constraints_nonsliding(model::Model, point, obstacle, β, λ, c_n, D, separation_from_obstacle, contact_velocity)
+#     D_transpose_times_v = [dot(d, contact_velocity) for d in D]
 
-    @constraints model begin
-        separation_from_obstacle <= 1e-3 # (7)
-        separation_from_obstacle >= -1e-3
-        contact_velocity.v .== 0
-        λ == 0 # (8)
-        obstacle.μ * c_n .- sum(β) >= 0 # (9)
-    end
+#     @constraints model begin
+#         separation_from_obstacle >= 0 # (7)
+#         # λ .+ D_transpose_times_v .>= 0 # (8)
+#         obstacle.μ * c_n .- sum(β) >= 0 # (9)
+#     end
 
-    ContactResult(β, λ, c_n, point, obstacle)
-end
+#     @disjunction(model, 
+#                  (&)(separation_from_obstacle == 0, 
+#                      [@?(contact_velocity.v[i] == 0) for i in 1:3]...),
+#                  c_n == 0)
+#     ContactResult(β, λ, c_n, point, obstacle)
+# end
+
+# function add_contact_constraints_sticking(model::Model, point, obstacle, β, λ, c_n, D, separation_from_obstacle, contact_velocity)
+
+#     @constraints model begin
+#         separation_from_obstacle <= 1e-3 # (7)
+#         separation_from_obstacle >= -1e-3
+#         contact_velocity.v .== 0
+#         λ == 0 # (8)
+#         obstacle.μ * c_n .- sum(β) >= 0 # (9)
+#     end
+
+#     ContactResult(β, λ, c_n, point, obstacle)
+# end
 
