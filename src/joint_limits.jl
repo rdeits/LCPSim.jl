4509d56219a2b07c3fcb77c270849ec41746872f
Robin Deits <robin.deits@gmail.com>
2017-12-01 19:54:04 -0500

    don't create empty @variable

--- src/joint_limits.jl
+++ src/joint_limits.jl
@@ -8,24 +8,28 @@ _upper(b::RigidBodyDynamics.Bounds) = b.upper
 function resolve_joint_limit(model::Model, xnext::LinearizedState, joint::Joint)
     bounds = RigidBodyDynamics.position_bounds(joint)
     N = length(bounds)
-    lb = _lower.(bounds)
-    ub = _upper.(bounds)
+    if N > 0
+        lb = _lower.(bounds)
+        ub = _upper.(bounds)
 
-    λ = @variable(model, [1:N], 
-                  lowerbound=-MAX_JOINT_FORCE, 
-                  upperbound=MAX_JOINT_FORCE, 
-                  basename="{λ_{joint}}")
-    q = current_configuration(xnext, joint)
+        λ = @variable(model, [1:N], 
+                      lowerbound=-MAX_JOINT_FORCE, 
+                      upperbound=MAX_JOINT_FORCE, 
+                      basename="{λ_{joint}}")
+        q = current_configuration(xnext, joint)
 
-    @constraints model begin
-        [i=1:N], q[i] >= lb[i]
-        [i=1:N], q[i] <= ub[i]
+        @constraints model begin
+            [i=1:N], q[i] >= lb[i]
+            [i=1:N], q[i] <= ub[i]
+        end
+
+        for i in 1:length(λ)
+            @disjunction(model, (q[i] >= ub[i]), (λ[i] >= 0))
+            @disjunction(model, (q[i] <= lb[i]), (λ[i] <= 0))
+        end
+
+        JointLimitResult(λ)
+    else
+        JointLimitResult(Vector{Variable}())
     end
-
-    for i in 1:length(λ)
-        @disjunction(model, (q[i] >= ub[i]), (λ[i] >= 0))
-        @disjunction(model, (q[i] <= lb[i]), (λ[i] <= 0))
-    end
-
-    JointLimitResult(λ)
 end
